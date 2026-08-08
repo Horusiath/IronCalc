29302f363183fe239a9c747382ebc580024b825b
Bartosz Sypytkowski <b.sypytkowski@gmail.com>
2026-08-08 06:28:24 +0200

    collab model patch application logic

--- base/src/new_empty.rs
+++ base/src/new_empty.rs
@@ -672,6 +672,7 @@ impl<'a> Model<'a> {
             tables: HashMap::new(),
             views,
             theme: Default::default(),
+            meta: Default::default(),
         };
         let parsed_formulas = Vec::new();
         let worksheets = &workbook.worksheets;
